f9b8de9bc24f285a054d05da8505937f42b4996b
nazarIvzhenko <nazarivzhenko@icloud.com>
2023-01-23 15:32:01 +0200

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Nazar Ivzhenko on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
